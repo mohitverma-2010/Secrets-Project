f96a4b7f8d8df117bcf7603f7efab4d8d9fee042
Mohit Verma <2107mohitverma@gmail.com>
2022-07-23 14:11:30 +0530

    Level 4 bcrypt Salting

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+//const md5 = require("md5");  //Level 3
 //const encrypt = require("mongoose-encryption");  //level 2
 
 const app = express();
@@ -39,44 +41,48 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const username = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({email: username}, function(err, foundUser){
-    if(err){
-      res.send(err);
-    }else{
-      if(foundUser){
-        res.send("Email already registered.");
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const username = req.body.username;
+    const password = hash;
+    User.findOne({email: username}, function(err, foundUser){
+      if(err){
+        res.send(err);
       }else{
-        const newUser = new User({
-          email: username,
-          password: password
-        });
-        newUser.save(function(err){
-          if(err){
-            console.log(err);
-          }else{
-            res.render("secrets");
-          }
-        });
+        if(foundUser){
+          res.send("Email already registered.");
+        }else{
+          const newUser = new User({
+            email: username,
+            password: password
+          });
+          newUser.save(function(err){
+            if(err){
+              console.log(err);
+            }else{
+              res.render("secrets");
+            }
+          });
+        }
       }
-    }
-  });
+    });
+});
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }else{
-          res.send("Password is incorrect!");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result==true){
+            res.render("secrets");
+          }else{
+            res.send("Password is incorrect!");
+          }
+        });
       }else{
         res.send("User not found, please register!");
       }
